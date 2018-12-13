1cdfc60d343d1725d134ae3514111d05b2a920d5
KyoshinSan <34346536+KyoshinSan@users.noreply.github.com>
2018-12-13 17:55:37 +0100

    UPDATE

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const db = require('sqlite')
 const moment = require('moment')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
+const methodOverride = require('method-override')
 
 let strDate = moment().format('DD[/]MM[/]YYYY')
 
@@ -14,8 +15,8 @@ app.set('view engine', 'pug')
 db.open('todolist.db').then(() => {
   console.log('Database Ready')
   Promise.all([
-  db.run('CREATE TABLE IF NOT EXISTS todos (message, completion, createdAt, updatedAt, userId)'),
-  db.run('CREATE TABLE IF NOT EXISTS users (firstname, lastname, username, password, email, createdAt, updatedAt)')
+    db.run('CREATE TABLE IF NOT EXISTS todos (message, completion, createdAt, updatedAt, userId)'),
+    db.run('CREATE TABLE IF NOT EXISTS users (firstname, lastname, username, password, email, createdAt, updatedAt)')
   ])
 }).then(() => {
   console.log('Tables Ready')
@@ -32,98 +33,10 @@ app.use(express.json())
 app.use(express.urlencoded({
  extended: true
 }))
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  console.log('-> ALL /')
-  res.redirect('/ressources')
-})
+app.use('/todos', require('./routes/todos'))
 
-app.get('/ressources', (req, res) => {
-  console.log('-> GET /ressources')
-  console.log('Database Open')
-  let json =[]
-  return db.all('SELECT * FROM todos')
-  .then(response => {
-    json = response
-  })
-  .then(() => {
-    let array_todos = []
-    let array_tmp = []
-    let i = 0
-
-    json.forEach(function(element) {
-      for (let key in element) {
-        array_tmp.push(element[key])
-      }
-      array_todos.push(array_tmp)
-      array_tmp = []
-    })
-    console.log(array_todos)
-    res.format({
-    'text/html': function() {
-      res.render('main', {
-        content: array_todos
-      })
-    },
-    'application/json': function(){
-      res.send(json)
-    }
-  })
-  })
-  .catch(err => console.log(err))
-})
-
-app.get('/ressources/:id', (req, res) => {
-  console.log('-> GET /ressources/:id (id : ' + req.params.id +')')
-  console.log('Database Open')
-  return db.get('SELECT * FROM todos WHERE rowid = ' + req.params.id)
-  .then(response => {
-    if(!(response === undefined || response === null)) {
-      res.json(response)
-    } else {
-      res.end('Aucune ligne trouvé !')
-    }
-  }).catch(err => console.log(err))
-})
-
-app.post('/ressources', (req, res) => {
-  console.log('-> POST /ressources')
-  console.log('Database Open')
-  return db.run(`INSERT into todos VALUES ('${req.body.message}', '${req.body.completion}', '${strDate}', '${strDate}', '${req.body.userId}')`)
-  .then(() => {
-    res.format({
-    'text/html': function() {
-      res.render('main', {
-        content: array_todos
-      })
-    },
-    'application/json': function(){
-      res.send({message: 'success'})
-    }
-  })
-  })
-  .catch(err => console.log(err))
-})
-
-app.delete('/ressources/:id', (req, res) => {
-  console.log('-> DELETE /ressources/:id (id : ' + req.params.id +')')
-  console.log('Database open')
-  return db.run('DELETE FROM todos WHERE rowid = ' + req.params.id)
-  .then(() => res.end('Donnée effacer'))
-  .catch(err => console.log(err))
-})
-
-app.put('/ressources/:id', (req, res) => {
-  console.log('-> PUT /ressources/:id (id : ' + req.params.id +')')
-  console.log('Database open')
-  if(req.body.message === undefined || req.body.message === null || req.body.completion === undefined || req.body.completion === null) {
-    res.end('Modification echouer')
-  } else {
-    return db.run(`UPDATE todos SET message = '${req.body.message}', completion = '${req.body.completion}', updatedAt = '${strDate}' WHERE rowid = ${req.params.todoId}`)
-    .then(() => res.end('Donnée modifier'))
-    .catch(err => console.log(err))
-  }
-})
 
 app.get('/ressources/add', (req, res) => {
   console.log('-> GET /ressources')
---
+++ routes/todos.js
@@ -0,0 +1,173 @@
+const router = require('express').Router()
+const db = require('sqlite')
+const moment = require('moment')
+let strDate = moment().format('DD[/]MM[/]YYYY')
+
+router.get('/', (req, res) => {
+  console.log('-> GET /todos')
+  console.log('Database Open')
+  let json = []
+  return db.all('SELECT * FROM todos')
+  .then(response => {
+    json = response
+  })
+  .then(() => {
+    let array_todos = []
+    let array_tmp = []
+    let i = 0
+
+    json.forEach(function(element) {
+      for (let key in element) {
+        array_tmp.push(element[key])
+      }
+      array_todos.push(array_tmp)
+      array_tmp = []
+    })
+    console.log(array_todos)
+    res.format({
+    'text/html': function() {
+      res.render('./todos/index', {
+        content: array_todos
+      })
+    },
+    'application/json': function(){
+      res.send(json)
+    }
+  })
+  })
+  .catch((err) => {
+    return res.status(404).send(err)
+  })
+})
+
+router.get('/add', (req, res) => {
+  console.log('-> GET /todos/add')
+  console.log('Database Open')
+  res.render('./todos/add')
+})
+
+router.get('/:id/edit', (req, res) => {
+  console.log('-> GET /todos/add')
+  console.log('Database Open')
+  res.render('./todos/')
+})
+
+router.get('/:id', (req, res, next) => {
+  console.log('-> GET /todos/:id (id : ' + req.params.id +')')
+  console.log('Database Open')
+  let json = []
+  return db.get('SELECT * FROM todos WHERE rowid = ' + req.params.id)
+  .then(response => {
+    if(!(response === undefined || response === null)) {
+      json = response
+    } else {
+      next()
+    }
+  })
+  .then(() => {
+    res.format({
+      'text/html': function() {
+        res.render('./todos/show', {
+          content: json,
+          strId: 'Todo de l\'id : ' + req.params.id
+        })
+      },
+      'application/json': function(){
+        res.send(json)
+      }
+    })
+  })
+  .catch((err) => {
+    return res.status(404).send(err)
+  })
+})
+
+router.post('/', (req, res) => {
+  console.log('-> POST /todos')
+  console.log('Database Open')
+  return db.run(`INSERT into todos VALUES ('${req.body.message}', '${req.body.completion}', '${strDate}', '${strDate}', '${req.body.userId}')`)
+  .then(() => {
+    res.format({
+      'text/html': function() {
+        res.redirect('/todos')
+      },
+      'application/json': function(){
+        res.send({message: 'success'})
+      }
+    })
+  })
+  .catch((err) => {
+    return res.status(404).send(err)
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  console.log('-> DELETE /todos/:id (id : ' + req.params.id +')')
+  console.log('Database open')
+  Promise.all([
+    db.get('SELECT * FROM todos WHERE rowid = ' + req.params.id),
+    db.run('DELETE FROM todos WHERE rowid = ' + req.params.id)
+  ])
+  .then((response) => {
+    if (response[0] === undefined || response[0] === null) {
+      res.format({
+        'text/html': function() {
+          res.redirect('/todos')
+        },
+        'application/json': function(){
+          res.send({message: 'failed'})
+        }
+      })
+    } else {
+      res.format({
+        'text/html': function() {
+          res.redirect('/todos')
+        },
+        'application/json': function(){
+          res.send({message: 'success'})
+        }
+      })
+    }
+  })
+  .catch((err) => {
+    return res.status(404).send(err)
+  })
+})
+
+router.put('/:id', (req, res) => {
+  console.log('-> PUT /todos/:id (id : ' + req.params.id +')')
+  console.log('Database open')
+  // verif req.body.message.length
+  if(req.body.message === undefined || req.body.message === null || req.body.completion === undefined || req.body.completion === null) {
+    res.end('Modification echouer')
+  } else {
+    Promise.all([
+    db.get('SELECT * FROM todos WHERE rowid = ' + req.params.id),
+    db.run(`UPDATE todos SET message = '${req.body.message}', completion = '${req.body.completion}', updatedAt = '${strDate}' WHERE rowid = ${req.params.id}`)
+  ]).then((response) => {
+  if (response[0] === undefined || response[0] === null) {
+    res.format({
+        'text/html': function() {
+          res.redirect('/todos')
+        },
+        'application/json': function(){
+          res.send({message: 'failed'})
+        }
+      })
+  } else {
+    res.format({
+      'text/html': function() {
+        res.redirect('/todos')
+      },
+      'application/json': function(){
+        res.send({message: 'success'})
+      }
+    })
+  }
+  }).catch((err) => {
+      return res.status(404).send(err)
+    })
+  }
+})
+
+module.exports = router
